010ade369d330728a007833816e0096113f03977
S4v!0r <gbp17@outlook.com>
2023-12-16 20:28:04 -0500

    Empezando el formulario

---
+++ vet/src/components/Form.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Patient Monitoring</h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            add patients and 
+            <span class="text-indigo-600 font-bold">manage them</span>
+        </p>
+    </div>
+</template>
